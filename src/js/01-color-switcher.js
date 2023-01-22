b7f74b20548556dc1ec1160010078b66c8d91ee6
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-01-22 19:23:25 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,9 +4,22 @@ function getRandomHexColor() {
 function bcgColor(){
 document.body.style.backgroundColor = getRandomHexColor()
 } 
+let intervalId = null;
 const btnStart = document.querySelector('button[data-start]')
 const btnStop = document.querySelector('button[data-stop]')
-btnStart,addEventListener('click', onBtnClick)
-function onBtnClick(e){
-setInterval(bcgColor, 1000);
+btnStop.disabled = true;
+btnStart.addEventListener('click', onBtnStartClick)
+
+function onBtnStartClick(e) {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+intervalId = setInterval(bcgColor, 1000);
+}
+
+btnStop.addEventListener('click', onBtnStopClick)
+
+function onBtnStopClick(e) {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+ clearInterval(intervalId)
 }
