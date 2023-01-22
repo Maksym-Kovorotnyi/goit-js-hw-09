e507881765937357821619b295cf7609bf00ceb3
Maksym-Kovorotnyi <116639382+Maksym-Kovorotnyi@users.noreply.github.com>
2023-01-22 17:08:42 +0000

    Deploying to gh-pages from @ Maksym-Kovorotnyi/goit-js-hw-09@6b7d54be059d1bdc8fd301bf1d6284245255fefb 🚀

---
+++ 01-color-switcher.12eae168.js
@@ -0,0 +1,2 @@
+function t(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}document.querySelector("button[data-start]"),document.querySelector("button[data-stop]");addEventListener("click",(function(o){setInterval(t,1e3)}));
+//# sourceMappingURL=01-color-switcher.12eae168.js.map
