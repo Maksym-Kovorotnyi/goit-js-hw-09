b9e607d7d7c9379442431a7be2eff16c7cb7c6c5
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-01-22 19:06:30 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function bcgColor(){
+document.body.style.backgroundColor = getRandomHexColor()
+} 
+const btnStart = document.querySelector('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+btnStart,addEventListener('click', onBtnClick)
+function onBtnClick(e){
+setInterval(bcgColor, 1000);
+}
